6a50148715e1459d71f3923b1e00d86efd651e06
suryanshp1 <suryanshp1@gmail.com>
2026-01-20 19:07:45 +0530

    feat: deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,28 @@
 // const express = require("express"); -> commonjs type
 import express from "express"; // module type import
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "Application is running"});
 });
+
+app.get("/books", (req, res) => {
+    res.status(200).json({message: "Books are coming soon"});
+});
+
+// make application ready for deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 app.listen(ENV.PORT, () => {
     console.log("Server is running on port ", ENV.PORT);
