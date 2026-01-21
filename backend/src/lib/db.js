d5a9fc3db926d83aeff02835fefd16c802ac0f8f
suryanshp1 <suryanshp1@gmail.com>
2026-01-21 12:52:49 +0530

    fix: Added exception

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,11 @@ import { ENV } from "./env.js";
 
 export const connectDb = async() => {
     try {
+
+        if(!ENV.DB_URL) {
+            throw new Error("DB_URL is not set");
+        }
+
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log(`Connected to MongoDB: ${conn.connection.host}`);
     } catch (error) {
